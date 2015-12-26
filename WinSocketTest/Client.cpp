7f1dfd0ae12569b447a0cbf7e6b91d2fd3fc5cf0
KaiYi.C <cg9250110@gmail.com>
2015-12-26 22:27:33 +0800

    almost done

--- WinSocketTest/Client.cpp
+++ WinSocketTest/Client.cpp
@@ -1,5 +1,6 @@
 #pragma comment(lib, "Ws2_32.lib")
 
+
 #include <WinSock2.h>
 #include <iostream>
 #include <string>
@@ -7,39 +8,61 @@
 #include<cstring>
 
 
+
 using namespace std;
+int r;
+SOCKET sConnect;
+string confirm;
+char message[200];
+fd_set fdread;
+int ret;
+string input;
+
+
+void readProc();
+void writeProc();
+
 
 void main()
 {
-	string confirm;
-	char message[200];
+
+
+
 
 	//開始 Winsock-DLL
-	int r;
+
 	WSAData wsaData;
 	WORD DLLVersion;
 	DLLVersion = MAKEWORD(2, 1);
 	r = WSAStartup(DLLVersion, &wsaData);
 
+
 	//宣告給 socket 使用的 sockadder_in 結構
 	SOCKADDR_IN addr;
 
+
 	int addlen = sizeof(addr);
 
+
+
 	//設定 socket
-	SOCKET sConnect;
+
+
 
 	//AF_INET: internet-family
 	//SOCKET_STREAM: connection-oriented socket
 	sConnect = socket(AF_INET, SOCK_STREAM, NULL);
 
+
 	//設定 addr 資料
-	addr.sin_addr.s_addr = inet_addr("192.168.100.161");
+	addr.sin_addr.s_addr = inet_addr("192.168.100.125");
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(25565);
 
+
 	cout << "connect to server?[Y] or [N]" << endl;
 	cin >> confirm;
+
 
 	if (confirm == "N")
 	{
@@ -51,36 +74,78 @@ void main()
 			cout << "Success";
 			connect(sConnect, (SOCKADDR*)&addr, sizeof(addr));
 			cout << "Success";
-			string input;
+
+
+
+
+
 			while (true) {
-				//接收 server 端的訊息
-				ZeroMemory(message, 200);
-				r = recv(sConnect, message, sizeof(message), 0);
-				cout << message << endl;
+
+
+
 
 				if (GetAsyncKeyState(83)) {
-					cout << "input";
-					cin >> input;
-					char *s_input = new char[input.size()+1];
-					strcpy(s_input, input.c_str());
-					send(sConnect, s_input, (int)strlen(s_input), 0);
-					
-
+					HANDLE writeHandle;
+					writeHandle = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)writeProc, NULL, 0, NULL);
+					WaitForSingleObject(writeHandle, INFINITE);
 				}
+				else {
+					HANDLE readHandle;
+					readHandle = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)readProc, NULL, 0, NULL);
+					WaitForSingleObject(readHandle, 100);
+					readHandle = FALSE;
+					//cout << "ELSE\n";
+				}
+
+
+				//WSAEVENT event = WSACreateEvent();
+				//WSANETWORKEVENTS eventt;
+				//::WSAEnumNetworkEvents(sConnect, event, &eventt);
+				//::WSAEventSelect(sConnect, event, FD_READ);
+				//if (eventt.lNetworkEvents & FD_READ)
+				//if ((ret=select(0, &fdread,NULL,NULL,NULL)>0)) {
+				/*if (FD_ISSET(sConnect,&fdread)){
+				r = recv(sConnect, message, sizeof(message), 0);
+				cout << message << endl;
+				FD_ZERO(&fdread);
+				}
+				*/
+
 
 				//設定 closesocket 時，不經過 TIME-WAIT 過程,直接關閉socket
 				//BOOL bDontLinger = FALSE;
 				//setsockopt(sConnect,SOL_SOCKET,SO_DONTLINGER,(const char*)&bDontLinger,sizeof(BOOL));
 
+
 				//若之後不再使用，可用 closesocket 關閉連線
+
 
 
 			}
 			closesocket(sConnect);
+
 
 			getchar();
 			getchar();
 		}
 	}
 
+
+}
+
+void readProc()
+{
+	//接收 server 端的訊息
+	ZeroMemory(message, 200);
+	r = recv(sConnect, message, sizeof(message), 0);
+	cout << message << endl;
+	//FD_ZERO(&fdread);
+	//cout << "listen\n";
+}
+void writeProc() {
+	cout << "input\n";
+	cin >> input;
+	char *s_input = new char[input.size() + 1];
+	strcpy(s_input, input.c_str());
+	send(sConnect, s_input, (int)strlen(s_input), 0);
 }
--- WinSocketTest/Server.cpp
+++ WinSocketTest/Server.cpp
@@ -1,31 +1,40 @@
 #pragma comment(lib, "Ws2_32.lib")
-
+#include <conio.h>
 #include <WinSock2.h>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+//開始 Winsock-DLL
+WSAData wsaData
+WORD DLLVSERION = MAKEWORD(2, 1);
+
+//用 WSAStartup 開始 Winsocket-DLL
+int r = WSAStartup(DLLVSERION, &wsaData);
+
+//宣告 socket 位址資訊(不同的通訊,有不同的位址資訊,所以會有不同的資料結構存放這些位址資訊)
+SOCKADDR_IN addr;
+int addrlen = sizeof(addr);
+
+//建立 socket
+SOCKET sListen;  //listening for an incoming connection
+SOCKET sConnect; //operating if a connection was found
+
+SOCKADDR_IN clinetAddr; //等待連線
+
+
+void getMessage();
+void sendMessage();
+void connecter();
+
+
 void main()
 {
-	int r;
-	WSAData wsaData;
-	WORD DLLVSERION;
-	DLLVSERION = MAKEWORD(2, 1);//Winsocket-DLL 版本
 
-								//用 WSAStartup 開始 Winsocket-DLL
-	r = WSAStartup(DLLVSERION, &wsaData);
 
-	//宣告 socket 位址資訊(不同的通訊,有不同的位址資訊,所以會有不同的資料結構存放這些位址資訊)
-	SOCKADDR_IN addr;
-	int addrlen = sizeof(addr);
-
-	//建立 socket
-	SOCKET sListen; //listening for an incoming connection
-	SOCKET sConnect; //operating if a connection was found
-
-					 //AF_INET：表示建立的 socket 屬於 internet family
-					 //SOCK_STREAM：表示建立的 socket 是 connection-oriented socket 
+	//AF_INET：表示建立的 socket 屬於 internet family
+	//SOCK_STREAM：表示建立的 socket 是 connection-oriented socket 
 	sConnect = socket(AF_INET, SOCK_STREAM, NULL);
 
 	//設定位址資訊的資料
@@ -36,31 +45,48 @@ void main()
 	//設定 Listen
 	sListen = socket(AF_INET, SOCK_STREAM, NULL);
 	bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
-	listen(sListen, SOMAXCONN);//SOMAXCONN: listening without any limit
+	listen(sListen, SOMAXCONN); //SOMAXCONN: listening without any limit
 
-							   //等待連線
-	SOCKADDR_IN clinetAddr;
+								//等待連線
+	cout << "Waiting for Connection..." << endl;
 	while (true)
 	{
-		cout << "Waiting for Connection..." << endl;
+		HANDLE connecterHandle;
+		connecterHandle = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)connecter, NULL, 0, NULL);
+		WaitForSingleObject(connecterHandle, 100);
 
-		if (sConnect = accept(sListen, (SOCKADDR*)&clinetAddr, &addrlen))
-		{
-			cout << "A Connection was Found" << endl;
-			cout << "Server: Got Connection From: " << inet_ntoa(addr.sin_addr) << endl;
-			//printf("server: got connection from %s\n", inet_ntoa(addr.sin_addr));
-
-			//傳送訊息給 client 端
-			while (1) {
-				string Msg;
-				cin >> Msg;
-				//string *sendbuf = Msg ;
-				send(sConnect, Msg.c_str(), (int)strlen(Msg.c_str()), 0);
-			}
-
-
+		if (GetAsyncKeyState(83)) {
+			HANDLE writeHandle;
+			writeHandle = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)getMessage, NULL, 0, NULL);
+			WaitForSingleObject(writeHandle, INFINITE);
+		}
+		else {
+			cout << "else here!";
+			HANDLE readHandle;
+			readHandle = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)sendMessage, NULL, 0, NULL);
+			WaitForSingleObject(readHandle, 100);
+			readHandle = FALSE;
 		}
 	}
 
-	//getchar();
+}
+void sendMessage() {
+	string Msg;
+	cin >> Msg;
+	send(sConnect, Msg.c_str(), (int)strlen(Msg.c_str()), 0);
+}
+void getMessage() {
+	char message[200];
+	ZeroMemory(message, 200);
+	r = recv(sConnect, message, sizeof(message), 0);
+	cout << message << endl;
+}
+void connecter() {
+	if (sConnect = accept(sListen, (SOCKADDR*)&clinetAddr, &addrlen))
+	{
+
+		cout << "A Connection was Found" << endl;
+		cout << "Server: Got Connection From: " << inet_ntoa(addr.sin_addr) << endl;
+		//printf("server: got connection from %s\n", inet_ntoa(addr.sin_addr));
+	}
 }
